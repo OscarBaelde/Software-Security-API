196e55889eefbb2cde2a38727d345cb4d2ebc4a2
Oscar Baelde <oscar.baelde@student.ehb.be>
2021-01-17 15:47:37 +0100

    Disallowing empty string to be submitted

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.post("/klachten", function (req, res) {
 
   let query = "INSERT INTO klachten SET ?";
   let text = { klacht: req.body.text };
+  if (text == "") res.send("String can't be empty");
   connection.query(query, text, function (err, result) {
     if (err) console.log(err);
     console.log(result);
